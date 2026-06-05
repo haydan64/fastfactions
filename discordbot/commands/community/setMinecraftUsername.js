b7fa93a4ed38cdec5f75f43333d95bb250871a85
Haydan64 <haydandfisher@gmail.com>
2026-06-04 18:20:06 -0600

    fixed an error with the setminecraftusername command that happens when a user enters a already usered username.

--- discordbot/commands/community/setMinecraftUsername.js
+++ discordbot/commands/community/setMinecraftUsername.js
@@ -12,8 +12,20 @@ module.exports = {
       return;
     }
 
-    
-    console.log("Player set gamertag", await upsertMinecraftProfile(interaction.user.id, username));
+    try {
+      await upsertMinecraftProfile(interaction.user.id, username);
+    } catch (err) {
+      if (err?.code === '23505' && err?.constraint === 'players_username_lower_idx') {
+        await interaction.reply({
+          content: `**${username}** is already linked to another Discord user. Please enter your own Minecraft username.`,
+          ephemeral: true
+        });
+        return;
+      }
+
+      throw err;
+    }
+
     await interaction.reply({
       content: `Saved your Minecraft username as **${username}**. We'll capture your XUID when you join the server.`,
       ephemeral: true
